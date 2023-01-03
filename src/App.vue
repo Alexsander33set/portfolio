6a1a72fd8f12060e85e469c362c61a2efe6d3fc9
Alexsander Patrick Ferreira Santos <alexsander.patrick@gmail.com>
2023-01-03 16:50:15 -0300

    Início -  Multilíngue
    Ínicio em funcionalidade para que detecte a linguagem do navegador do usuário e muda a página para tal, mas sem tirar a possibilidade de altera-la manualmente.

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,31 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
+  <button @click="changePageLanguage">mudar linguagem padrão pra portugues brasileiro</button>
   <router-view/>
 </template>
 
 <script>
 export default {
-  created() {
-    
+data(){return{userPreferedLanguage:'',}},
+created() {
+    this.checkLanguages()
+   
+},
+methods:{
+  checkLanguages(){
+    //SE NÃO TIVER O USER LANGUAGE NO LOCAL STORAGE, DEFINE POR PADRÃO DO BROWSER
+    if(!localStorage.getItem('userLanguage')){
+      localStorage.setItem('userLanguage', navigator.language)
+    }
+    this.userPreferedLanguage = localStorage.getItem('userLanguage')
+
+    console.log(' Preferência de linguagem usuário - LocalStorage: '+this.userPreferedLanguage)
   },
+  changePageLanguage(){
+    localStorage.setItem('userLanguage', 'pt-BR')
+  }
+}
 }
 </script>
 <style lang="scss">
@@ -32,6 +49,22 @@ nav {
     &.router-link-exact-active {
       color: #42b983;
     }
+  }
+}
+button{
+  padding:.5rem;
+  text-transform:capitalize;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-weight: 600;
+  box-shadow: inset 0 0 2px 0px #42b983;
+  border: none;
+  border-radius: .5rem;
+  cursor: pointer;
+
+  &:hover{
+    background: #42b983;
+    outline: 2px solid #c6c6c6;
+    outline-offset: 2px;
   }
 }
 </style>
