d3e40ba359571e49e6513db8e4493ee9888630c4
Alexsander Patrick Ferreira Santos <alexsander.patrick@gmail.com>
2023-02-11 18:47:51 -0300

    _

--- src/views/WeatherAPI/Homepage.vue
+++ src/views/WeatherAPI/Homepage.vue
@@ -1,31 +1,55 @@
 <template>
   <div class="mainBody">
-    <input type="text" placeholder="Nome da Cidade" v-model="userInputs.city" />
-    <input type="text" placeholder="Código do estado" v-model="userInputs.stateCode" />
-    <input type="text" placeholder="Código do país" v-model="userInputs.countryCode" />
-    <input type="text" placeholder="Limite" v-model="limit" required="true" />
-    <button @click="getCoordinates()">try get API</button>
-    <div><button @click="getWeather()">try get API Weather</button></div>
-    <br />
+    <button @click="getCoordinates()">try get API</button><br><br>
+    <button @click="getWeather()">try get API Weather</button>
+    <br><br>
     <hr /><br />
     <button @click="getCity()">Location</button>
     <input type="text" disabled="true" v-model="geocityLocation" />
     <br>
     <hr /><br>
-    <div class="weatherInfo">
+    <div class="weatherInfo" v-if="this.bruteWeatherData.cod==200">
       <h1>getAPIData</h1><br>
 
       <h5>Location: <p>{{ this.bruteWeatherData.name }} - Longitude: {{ this.lon ? this.lon : "Sem acesso" }} | Latitude:
           {{ this.lat ? this.lat : "Sem acesso" }}</p>
       </h5>
-      <div class="containerTemp">
+      <div class="weatherContainer">
+        <article class="weatherCard">
         <img src="http://openweathermap.org/img/wn/10d@2x.png" alt=""/>
         <div class="tempData">
           <div>{{ (this.bruteWeatherData.main.temp-273.15).toFixed(2)}}<!-- º C | º F | --> º C</div>
           <div class="minMax"><p>{{"Min: "+ (this.bruteWeatherData.main.temp_min-273.15).toFixed(2)}} º</p><p>Max: {{ (this.bruteWeatherData.main.temp_max-273.15).toFixed(2) }} º</p></div>
         </div>
-      </div>     
+        <div class="selectMetricUnit" style="gap:12px;">
+            <div style="font-size:1rem; user-select: none; cursor:pointer;" @click="this.userWeatherPreferences.metricUnit='C'">°C</div>
+            <div style="font-size:1rem; user-select: none; cursor:pointer;" @click="this.userWeatherPreferences.metricUnit='F'">°F</div>
+            <div style="font-size:1rem; user-select: none; cursor:pointer;" @click="this.userWeatherPreferences.metricUnit='K'"> °K</div>
+        </div>
+      </article>     
+      <article class="weatherCard">
+        <div class="tempData" style="font-size:1rem;">
+          <div>Pressão: {{ this.bruteWeatherData.main.pressure}} mb</div>
+        </div>
+      </article> 
+      <article class="weatherCard">
+        <div class="tempData" style="font-size:1rem;">
+          <div>Humidade: {{ this.bruteWeatherData.main.humidity}}%</div>
+        </div>
+      </article>
+      <article class="weatherCard">
+        <div class="tempData" style="font-size:1rem;">
+          <div>Visibilidade: {{ (this.bruteWeatherData.visibility/100) + "%"}}</div>
+        </div>
+      </article>
+      <article class="weatherCard">
+        <div class="tempData" style="font-size:1rem;">
+          <div>Velocidade do vento: {{ this.bruteWeatherData.wind.speed + "m/h"}}</div>
+        </div>
+      </article>
+      </div>    
     </div>
+    <template v-else>Ops! alguem erro ocorreu...</template>
   </div>
 </template>
 <script>
@@ -85,7 +109,7 @@ export default {
         "cod": 200
       },
       userInputs: { city: "", stateCode: "", countryCode: "" },
-      limit: "",
+      limit: 1,
       lat: "", lon: "",
       geoCity: "",
       geocityLocation: '',
@@ -95,37 +119,18 @@ export default {
     this.getGeolocatization();
   },
   methods: {
-    showUserPreference() { console.log(this.preferedLanguage) },
     getGeolocatization() {
-      const successCallback = (position) => {
+      let successCallback = (position) => {
         console.log(position);
-        this.lat = position.coords.latitude.toFixed(2);
-        this.lon = position.coords.longitude.toFixed(2);
+        this.lon = position.coords.latitude.toFixed(2);
+        this.lat = position.coords.longitude.toFixed(2);
       };
-      const errorCallback = (error) => {
+      let errorCallback = (error) => {
         console.log(error);
       };
-
       navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-    },
-    getCoordinates() {
-      //https://openweathermap.org/api/geocoding-api
-      const apiCallByCity =
-        "http://api.openweathermap.org/geo/1.0/direct?q=" + this.userInputs.city + "&limit=" + this.limit + "&appid=";
-      const apiCallByStateCode =
-        "http://api.openweathermap.org/geo/1.0/direct?q=" +
-        this.userInputs.stateCode + "&limit=" + this.limit + "&appid=";
-      const apiCallByCountryCode =
-        "http://api.openweathermap.org/geo/1.0/direct?q=" +
-        this.userInputs.countryCode + "&limit=" + this.limit + "&appid=";
-      console.log(
-        "ByStateCode:" + apiCallByStateCode + "\nByCountryCode: " + apiCallByCountryCode + "\nByCityName: " + apiCallByCity
-      );
-      axios
-        .get(apiCallByCountryCode + process.env.VUE_APP_API_KEY)
-        .then((response) => {
-          console.log(response.data);
-        });
+
+      
     },
     getWeather() {
       axios
@@ -146,15 +151,25 @@ export default {
         .then((response) => {
           console.log(response.data);
           this.geoCity = response.data;
-          this.geocityLocation = response.data[0].name + ', ' + response.data[0].state
+          this.geocityLocation = response.data.name + ', ' + response.data.state
         });
+    },
+    temperatureConverting(converting, temperature=Number){
+      converting
+      if (!temperature) {return}
+      converting=='kelvin2ceusius' ? temperature-272 : '';
     },
   },
 };
 </script>
 
 <style>
-.containerTemp{
+
+.weatherContainer{
+  display: grid;
+  grid-template-columns: repeat( auto-fit, minmax(400px, 1fr));
+}
+.weatherCard{
   background: rgba(100,200,255,0.2);
   box-shadow: 2px 2px 8px -1px black;
   border-radius:16px;
@@ -167,10 +182,10 @@ export default {
   font-size: 2.7rem;
   max-width:max-content;
 }
-.containerTemp img{
+.weatherCard img{
       height: 120%;
     }
-    .containerTemp .tempData .minMax{
+    .weatherCard .tempData .minMax{
       font-size: .725rem;
       display: flex;
       align-items: center;
