7d80d352b2995ac06ce531384615ecc75a46e369
Alexsander Patrick Ferreira Santos <alexsander.patrick@gmail.com>
2023-02-13 17:16:17 -0300

    1_

--- src/App.vue
+++ src/App.vue
@@ -51,14 +51,17 @@ export default {
 };
 </script>
 <style lang="scss"> 
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
+html,body,.mainBody{width: 100dvw; height: 100dvh;}
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family:Inter, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   min-height: 100dvh;
--- src/views/WeatherAPI/Homepage.vue
+++ src/views/WeatherAPI/Homepage.vue
@@ -52,17 +52,41 @@
   </div>
     <template v-else>Ops! alguem erro ocorreu...</template> -->
 
-    <div class="mainBody">
-      <div class="navbar">Navbar</div>
-      <main class="mainContainer">
-        Main
-      </main>
-      <footer>Footer</footer>
-
-    </div>
-
-
-
+  <div class="mainBody">
+    <div class="navbar">Navbar</div>
+    <main class="mainContainer">
+      <div>Atualizado em 0m</div>
+      <div class="weatherIcon"><img :src="'http://openweathermap.org/img/wn/'+this.bruteWeatherData.weather[0].icon+'@2x.png'" :alt="this.bruteWeatherData.weather[0].description" ></div>
+      <div class="weatherCity">{{ this.bruteWeatherData.name }}</div>
+      <div class="mainTemperature">
+        <div class="averageTemp">{{ (this.bruteWeatherData.main.temp-273.15).toFixed(2)}} º C</div>
+        <div class="maxMinTemp">
+          <div class="maxTemp"><i></i><p>↑{{(this.bruteWeatherData.main.temp_max-273.15).toFixed(2)}} ºC</p></div>
+          <div class="maxTemp"><i></i><p>↓{{(this.bruteWeatherData.main.temp_min-273.15).toFixed(2)}} ºC</p></div>
+        </div>
+      </div>
+      <div class="tempTex">{{ this.bruteWeatherData.weather[0].description }}</div>
+      <div class="cardsContainer">
+        <article class="card grid2">
+          <h6>Sensaçào Térmica</h6>
+          <p>{{ this.bruteWeatherData.main.feels_like }}</p>
+        </article>
+        <article class="card">
+          <h6>Visibilidade</h6>
+          <p>{{ this.bruteWeatherData.visibility/100 }} %</p>
+        </article>
+        <article class="card">
+          <h6>Humidade</h6>
+          <p>{{ this.bruteWeatherData.main.humidity }} %</p>
+        </article>
+        <article class="card">
+          <h6>Vento</h6>
+          <p>{{ this.bruteWeatherData.wind.speed }} m/s</p>
+        </article>
+      </div>
+    </main>
+    <footer>Footer</footer>
+  </div>
 </template>
 <script>
 import axios from "axios";
@@ -70,61 +94,62 @@ export default {
   name: "WeatherHome",
   data() {
     return {
-      userWeatherPreferences: { metricUnit: '', },
-      bruteWeatherData:
-      {
-        "coord": {
-          "lon": -0.13,
-          "lat": 51.51
+      userWeatherPreferences: { metricUnit: "" },
+      bruteWeatherData: {
+        coord: {
+          lon: 10.99,
+          lat: 44.34,
         },
-
-        "weather": [
-
+        weather: [
           {
-            "id": 300,
-            "main": "Drizzle",
-            "description": "light intensity drizzle",
-            "icon": "09d"
-          }
+            id: 501,
+            main: "Rain",
+            description: "moderate rain",
+            icon: "10d",
+          },
         ],
-        "base": "stations",
-
-        "main": {
-          "temp": 280.32,
-          "pressure": 1012,
-          "humidity": 81,
-          "temp_min": 279.15,
-          "temp_max": 281.15
+        base: "stations",
+        main: {
+          temp: 298.48,
+          feels_like: 298.74,
+          temp_min: 297.56,
+          temp_max: 300.05,
+          pressure: 1015,
+          humidity: 64,
+          sea_level: 1015,
+          grnd_level: 933,
         },
-        "visibility": 10000,
-
-        "wind": {
-          "speed": 4.1,
-          "deg": 80
+        visibility: 10000,
+        wind: {
+          speed: 0.62,
+          deg: 349,
+          gust: 1.18,
         },
-
-        "clouds": {
-          "all": 90
+        rain: {
+          "1h": 3.16,
         },
-        "dt": 1485789600,
-
-        "sys": {
-          "type": 1,
-          "id": 5091,
-          "message": 0.0103,
-          "country": "GB",
-          "sunrise": 1485762037,
-          "sunset": 1485794875
+        clouds: {
+          all: 100,
         },
-        "id": 2643743,
-        "name": "London",
-        "cod": 200
+        dt: 1661870592,
+        sys: {
+          type: 2,
+          id: 2075663,
+          country: "IT",
+          sunrise: 1661834187,
+          sunset: 1661882248,
+        },
+        timezone: 7200,
+        id: 3163858,
+        name: "Zocca",
+        cod: 200,
       },
       userInputs: { city: "", stateCode: "", countryCode: "" },
       limit: 1,
-      lat: "", lon: "",
+      lat: "",
+      lon: "",
       geoCity: "",
-      geocityLocation: '',
+      geocityLocation: "",
     };
   },
   created() {
@@ -141,83 +166,110 @@ export default {
         console.log(error);
       };
       navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-
-      
     },
     getWeather() {
       axios
         .get(
-          "https://api.openweathermap.org/data/2.5/weather?lat="
-          + this.lat + "&lon=" + this.lon + "&appid=" + process.env.VUE_APP_API_KEY)
+          "https://api.openweathermap.org/data/2.5/weather?lat=" +
+            this.lat +
+            "&lon=" +
+            this.lon +
+            "&appid=" +
+            process.env.VUE_APP_API_KEY
+        )
         .then((response) => {
           console.log(response.data);
           this.bruteWeatherData = response.data;
-          console.log(this.bruteWeatherData)
+          console.log(this.bruteWeatherData);
         });
     },
     getCity() {
       axios
-        .get("http://api.openweathermap.org/geo/1.0/reverse?lat=" +
-          this.lat + "&lon=" + this.lon + "&limit=1&appid=" + process.env.VUE_APP_API_KEY
+        .get(
+          "http://api.openweathermap.org/geo/1.0/reverse?lat=" +
+            this.lat +
+            "&lon=" +
+            this.lon +
+            "&limit=1&appid=" +
+            process.env.VUE_APP_API_KEY
         )
         .then((response) => {
           console.log(response.data);
           this.geoCity = response.data;
-          this.geocityLocation = response.data.name + ', ' + response.data.state
+          this.geocityLocation =
+            response.data.name + ", " + response.data.state;
         });
     },
-    temperatureConverting(converting, temperature=Number){
-      converting
-      if (!temperature) {return}
-      converting=='kelvin2ceusius' ? temperature-272 : '';
+    temperatureConverting(converting, temperature = Number) {
+      converting;
+      if (!temperature) {
+        return;
+      }
+      converting == "kelvin2ceusius" ? temperature - 272 : "";
     },
   },
 };
 </script>
 
-<style>
-
-.navbar{
+<style >
+.mainBody {background: #E5ECF4}
+.navbar {
   height: 56px;
+  background: rgba(0, 128, 0, 0.13);
 }
-
-
-
-
-
-
-
-
-
-
-@media (min-width: 1024px){
-  .weatherContainer{
+.cardsContainer{
   display: grid;
-  grid-template-columns: repeat( auto-fit, minmax(400px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(96px, 1fr));
+  margin: 12px;
+  background: rgba(255, 255, 0, 0.425);
+  gap: 12px;
 }
-.weatherCard{
-  background: rgba(100,200,255,0.2);
-  box-shadow: 2px 2px 8px -1px black;
-  border-radius:16px;
-  margin: 24px 8px;
-  padding: 1rem 2rem;
-  display:flex;
-  align-items: center;
-  justify-content: center;
-  gap: 24px;
-  font-size: 2.7rem;
-  max-width:max-content;
+.card{background: white;
+  border-radius: 10px;
+  text-align: center;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.25);
+  height: 96px;
+  padding: 8px;
 }
-.weatherCard img{
-      height: 120%;
-    }
-    .weatherCard .tempData .minMax{
-      font-size: .725rem;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      gap: 24px;
-    }
+.card h6{
+  font-weight: 400;
+  font-size: 14;
+  color: #363636;
 }
+.card p{
+  font-weight: 500;
+  font-size: 20;
+  color: #444444;
+}
+.grid2{grid-column: 1/3;}
 
+@media (min-width: 1024px) {
+  .weatherContainer {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  }
+  .weatherCard {
+    background: rgba(100, 200, 255, 0.2);
+    box-shadow: 2px 2px 8px -1px black;
+    border-radius: 16px;
+    margin: 24px 8px;
+    padding: 1rem 2rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 24px;
+    font-size: 2.7rem;
+    max-width: max-content;
+  }
+  .weatherCard img {
+    height: 120%;
+  }
+  .weatherCard .tempData .minMax {
+    font-size: 0.725rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 24px;
+  }
+}
 </style>
