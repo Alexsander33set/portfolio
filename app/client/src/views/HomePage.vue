c90bfc0e7b2b7cd097cd0bfc10760b6fd8977709
Alexsander P.F. Santos <82063944+Alexsander33set@users.noreply.github.com>
2024-07-24 21:20:42 -0300

    change pages name

---
+++ app/client/src/views/HomePage.vue
@@ -0,0 +1,25 @@
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+
+const response = ref([])
+
+function getProjects(){
+  axios.get("/api/projects")
+    .then((res)=>{
+      console.log(res)
+      response.value = res
+  })
+}
+</script>
+
+<template>
+  <main>
+    <hr>
+    <h1 align="center">My Portfolio</h1>
+    <button @click="getProjects">Botão</button>
+    <hr>
+    <h3>Body</h3>
+    {{ response }}
+  </main>
+</template>
--- app/client/src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup></script>
-
-<template>
-  <main>
-    <hr>
-    <h1 align="center">My Portfolio</h1>
-    <hr>
-  </main>
-</template>
