7f5d5d677536241c534682bf5779f40f68b6027e
Alexsander P.F. Santos <82063944+Alexsander33set@users.noreply.github.com>
2024-08-06 12:12:11 -0300

    Update(client): change structure

--- app/client/src/App.vue
+++ app/client/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import Navbar from './components/Navbar.vue'
-import FooterComponent from './components/FooterComponent.vue'
+import Navbar from './components/layout/Navbar.vue'
+import FooterComponent from './components/layout/FooterComponent.vue'
 </script>
 
 <template>
