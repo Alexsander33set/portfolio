58536c6d70a5093f1092813da1edb2ad3937927d
Sander <82063944+Alexsander33set@users.noreply.github.com>
2024-08-18 09:09:59 -0300

    add Privacy and Acceptance page

--- app/client/src/router/index.js
+++ app/client/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomePage.vue'
 import ProjectPage from '../views/ProjectPage.vue'
+import PrivacyandAcceptanceTerms from '../views/PrivacyandAcceptanceTerms.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +18,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: ProjectPage
+    },
+    {
+      path: '/:language/privacy-&-acceptance-terms',
+      name: 'Privacy and Acceptance Terms',
+      component: PrivacyandAcceptanceTerms
     }
   ]
 })
