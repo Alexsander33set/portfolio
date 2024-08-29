641da701e783cea0399870d078a72e05515e36ae
Sander <82063944+Alexsander33set@users.noreply.github.com>
2024-08-29 06:23:00 -0300

    client: add mockup

---
+++ app/client/src/components/ProjectList.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { defineProps } from 'vue'
+import { Button } from '@/components/ui/button'
+
+const props = defineProps({
+  projects: {
+    type: Array,
+    required: true
+  },
+  title: {
+    type: String,
+    default: 'Projetos'
+  }
+})
+</script>
+
+<template>
+  <div class="project-list-container">
+    <h2 v-if="title">{{ title }}</h2>
+    <div class="project-list">
+      <div v-for="project in projects" :key="project._id.$oid" class="project-item">
+        <h3>{{ project.name }}</h3>
+        <p>{{ project.description }}</p>
+        <p>Tecnologias: {{ project.technologies.join(', ') }}</p>
+        <a :href="'https://'+project.url" target="_blank" rel="noopener noreferrer">
+          <Button variant="outline">{{ $t('projects.view') }}</Button>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.project-list-container {
+  margin-top: 1rem;
+}
+
+.project-list {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 1rem;
+  margin-top: 1rem;
+}
+
+.project-item {
+  border: 1px solid #ccc;
+  padding: 1rem;
+  border-radius: 8px;
+}
+</style>
--- app/client/src/views/HomePage.vue
+++ app/client/src/views/HomePage.vue
@@ -1,14 +1,59 @@
 <script setup>
 import axios from 'axios'
 import { ref } from 'vue'
+import { Button } from '@/components/ui/button'
+import ProjectList from '@/components/ProjectList.vue'
 
-const response = ref([])
+const projects = ref([
+    {
+        "_id": "66baecd6ed46e383b86a5484",
+        "name": "Weather Forecast",
+        "slug": "weather-forecast",
+        "description": "something",
+        "technologies": [
+            "java",
+            "node",
+            "python"
+        ],
+        "url": "www.apfs.com.br",
+        "is_private": false,
+        "created_at": 1723526343.82374
+    },
+    {
+        "_id": "66baf3812ffa799538467a33",
+        "name": "Weather Forecast",
+        "slug": "weather-forecast",
+        "description": "something",
+        "technologies": [
+            "java",
+            "node",
+            "python"
+        ],
+        "url": "www.apfs.com.br",
+        "is_private": false,
+        "created_at": 1723528060
+    },
+    {
+        "_id": "66baf3b55b1172f323a2cc99",
+        "name": "Weather Forecast",
+        "slug": "weather-forecast",
+        "description": "something",
+        "technologies": [
+            "java",
+            "node",
+            "python"
+        ],
+        "url": "www.apfs.com.br",
+        "is_private": false,
+        "created_at": 1723528108
+    }
+])
 
 function getProjects(){
   axios.get("/api/projects")
     .then((res)=>{
       console.log(res)
-      response.value = res
+      projects.value = res.data
   })
 }
 </script>
@@ -16,10 +61,9 @@ function getProjects(){
 <template>
   <main>
     <hr>
-    <h1 align="center">My Portfolio</h1>
-    <button @click="getProjects">Botão</button>
+    <h1 align="center">Meu Portfólio</h1>
+    <Button @click="getProjects" variant="outline" class="m-4">Obter projetos</Button>
     <hr>
-    <h3>Body</h3>
-    {{ response }}
+    <ProjectList :projects="projects" :title="$t('projects.title')" />
   </main>
 </template>
