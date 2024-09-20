878ef05f07eb72ea9bcd46768cd2ac6a4217fbe9
Alexsander Patrick Ferreira Santos <82063944+Alexsander33set@users.noreply.github.com>
2024-09-20 13:53:55 -0300

    fix server: change mongoDB connection

--- app/server/models/Mongo.py
+++ app/server/models/Mongo.py
@@ -1,6 +1,8 @@
+import logging
 import os
 from typing import Any, Dict
 from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 from pymongo.database import Database
 
 #* =====================================================
@@ -22,12 +24,19 @@ if not MONGO_DB_NAME:
 #* priority
 #* is_private
 #* created_at
-#*
+#* status
 
     
 class MongoDB:
     def __init__(self, collection_name: str):
-        self.client: MongoClient[Dict[str, Any]] = MongoClient(MONGO_URL)
+        self.client: MongoClient[Dict[str, Any]] = MongoClient(MONGO_URL, server_api=ServerApi('1'))
+        
+        try:
+            self.client.admin.command('ping')
+            logging.info("Successfully connected to MongoDB!")
+        except Exception as e:
+            print(e)
+        
         self.db = self.client[MONGO_DB_NAME]
         self.collection = self.db[collection_name]
 
