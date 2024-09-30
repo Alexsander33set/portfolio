fb8c880fb5919ea40652b9325037228e545de936
Sander <82063944+Alexsander33set@users.noreply.github.com>
2024-09-30 19:36:16 -0300

    client: changes
    WIP

--- app/client/src/views/HomePage.vue
+++ app/client/src/views/HomePage.vue
@@ -5,6 +5,20 @@ import { Button } from '@/components/ui/button'
 import { Icon } from '@iconify/vue'
 import ProjectList from '@/components/ProjectList.vue'
 
+const TOFisOpen = ref(false)
+
+async function getProjects(){
+  await axios.get("/api/projects")
+    .then((res)=>{
+      if (res.status != 200 || res.headers['Content-Type'] != "application/json"){
+        console.warn(">> Invalid Request <<")
+        return null
+      } else {return res.data}
+  }).catch((err) =>{
+    console.error(err)
+  })
+}
+
 /** mockup */
 const projects = ref([
     {
@@ -51,57 +65,44 @@ const projects = ref([
     }
 ])
 
-const TOFisOpen = ref(false)
+projects.value = getProjects()
 
-function getProjects(){
-  axios.get("/api/projects")
-    .then((res)=>{
-      console.log(res)
-      projects.value = res.data
-  })
-}
 
-// 
-try {
-  getProjects()
-} catch (error) {
-  console.error(error)
-}
 </script>
 
 <template>
   <main class="relative scroll-smooth scroll-pt-12 overflow-x-hidden">
-    <section id="presentation" class="pt-20 container flex flex-col justify-center relative">
-      <h1 class="text-4xl text-4xl font-bold dark:text-white">Alexsander Patrick Ferreira Santos</h1>
+    <section id="presentation" class="mt-20 container flex flex-col justify-center relative">
+      <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Alexsander Patrick Ferreira Santos</h1>
       <h2 class="text-xl">{{ $t('presentation.subtitle') }}</h2>
       <p class="py-4 mb-4 w-1/2 text-lg font-normal text-gray-500 dark:text-gray-400">
         {{ $t('presentation.description') }}
       </p>
     </section>
 
-    <section id="skills" class="container">
+    <section id="skills" class="container mt-12">
       <h2 class="text-xl">{{ $t('skills.title') }}</h2>
-      <div class="flex flex-wrap gap-2 items-center mt-4 text-gray-700">
-        <article class="flex items-center gap-2 py-2 px-4 text-lg rounded-sm border">
+      <div class="flex flex-wrap gap-4 items-center mt-4 text-gray-700">
+        <article class="flex items-center gap-2 py-4 px-8 text-lg rounded-sm border">
             <Icon icon="mdi:eye-outline" /> {{ $t('skills.list.accessibility') }}
         </article>
-        <article class="flex items-center gap-2 py-2 px-4 text-lg rounded-sm border">
+        <article class="flex items-center gap-2 py-4 px-8 text-lg rounded-sm border">
             <Icon icon="mdi:shield-check-outline" /> {{ $t('skills.list.security') }}
         </article>
-        <article class="flex items-center gap-2 py-2 px-4 text-lg rounded-sm border">
+        <article class="flex items-center gap-2 py-4 px-8 text-lg rounded-sm border">
             <Icon icon="mdi:server" /> {{ $t('skills.list.devops') }}
         </article>
       </div>
-      <p class="text-xl mt-4">{{ $t('skills.description') }}</p>
+      <p class="text-base mt-4">{{ $t('skills.description') }}</p>
     </section>
 
-    <ProjectList id="projects" class="container"
+    <ProjectList v-if="projects" id="projects" class="container mt-12"
       :projects="projects"
       :title="$t('projects.title')" />
 
-    <section id="contacts" class="mt-12 container flex flex-col items-center justify-center">
-      <h2>{{ $t('contacts.title') }}</h2>
-      <div class="flex gap-2">
+    <section id="contacts" class="mt-12 container flex items-center justify-center">
+      <h2 class="text-xl">{{ $t('contacts.title') }}:</h2>
+      <span class="flex gap-2">
         <a href="https://www.linkedin.com/in/alexsander-patrick-ferreira-santos-5ba0841ba/" target="_blank">
           <Button variant="outline" class="m-2">
               <Icon icon="fa6-brands:linkedin" />
@@ -114,11 +115,12 @@ try {
               &nbsp;Alexsander33set
           </Button>
         </a>
-      </div>
+      </span>
     </section>
 
-    <div class="absolute top-20 -right-4 hover:-right-2 p-2 hover:scale-105 cursor-pointer border rounded-sm text-center duration-300 hover:shadow-md opacity-50 hover:opacity-100 transition-all"
-      @click="TOFisOpen = !TOFisOpen" @mouseleave="TOFisOpen = false"
+    <div class="absolute top-20 -right-2 hover:-right1 p-2 hover:scale-105 cursor-pointer border rounded-sm text-center duration-300 hover:shadow-md opacity-50 hover:opacity-100 transition-all"
+      @click="TOFisOpen = !TOFisOpen"
+      @mouseleave="TOFisOpen = false"
     >
       <Icon icon="mdi:menu-left" class="scale-150 transition-all duration-300" :class="{'rotate-180': TOFisOpen}" />
       <div id="tableOfContents" v-if="TOFisOpen"
