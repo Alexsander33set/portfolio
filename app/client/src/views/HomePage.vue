e3d5ed59fd41688c112b07e87e15634c4ebca51f
Sander <82063944+Alexsander33set@users.noreply.github.com>
2024-10-08 23:13:04 -0300

    Fix[`client`]: remove localhost (#5)

--- app/client/src/views/HomePage.vue
+++ app/client/src/views/HomePage.vue
@@ -24,7 +24,7 @@ const projects = ref([])
 
 async function getProjects(){
   try{
-    let response = await axios.get("http://127.0.0.1:8080/api/projects")
+    let response = await axios.get("/api/projects")
       console.log(response)
       if (response.status != 200 || response['headers']['content-type'] != "application/json"){
         console.warn(">> Invalid Request <<")
