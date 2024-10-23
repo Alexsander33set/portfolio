cda04e88c4e1d472d9038da858899c29f18b394d
Alexsander Patrick Ferreira Santos <82063944+Alexsander33set@users.noreply.github.com>
2024-10-23 16:16:38 -0300

    WIP client[ProjectList]: add skeleton when loading

--- app/client/src/components/ProjectList.vue
+++ app/client/src/components/ProjectList.vue
@@ -19,7 +19,8 @@ const props = defineProps({
     <h2 class="text-lg font-bold my-4" v-if="title">{{ title }}</h2>
     <div class="flex flex-col gap-4">
 
-      <div v-for="(project, index) in props.projects" :key="index"
+      <template v-if="projects.length > 0">
+        <div v-for="(project, index) in props.projects" :key="index"
         class="shadow-[0_0px_3px_0px_rgba(0,0,0,0.3)] p-4 rounded-md">
         <h3>{{ project.name }}</h3>
         <p>{{ project.description }}</p>
@@ -35,6 +36,22 @@ const props = defineProps({
           </a>
         </div>
       </div>
+      </template>
+      <template v-else>
+        <div class="shadow-[0_0px_3px_0px_rgba(0,0,0,0.3)] p-4 rounded-md">
+          <h3>project.name</h3>
+          <p> project.description </p>
+          <p>Tecnologias:  project.technologies.join(', ') </p>
+          <div class="flex gap-2 mt-4">
+            <a href="#" target="_blank" rel="noopener noreferrer" class="w-full">
+              <Button variant="outline"> projects.github </Button>
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <Button variant="outline"> projects.view </Button>
+            </a>
+        </div>
+      </div>
+      </template>
     </div>
   </div>
 </template>
