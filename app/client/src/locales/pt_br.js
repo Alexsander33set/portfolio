5e98ba88de13ed53b505a20a3414376aaabdf90f
Sander <82063944+Alexsander33set@users.noreply.github.com>
2024-12-28 13:06:13 -0300

    Client: Add translations

--- app/client/src/locales/pt_br.js
+++ app/client/src/locales/pt_br.js
@@ -1,11 +1,37 @@
 export const pt_br = {
+  links:{
+    about: 'Sobre mim',
+  },
   theme: {
     toggle: 'Alternar tema',
     light: 'Claro',
     dark: 'Escuro',
     system: 'Sistema'
   },
+  welcomePhases:{
+    dawn:[
+      "Noite tranquila à vista! Vamos finalizar com sucesso?",
+      "Hora de desacelerar, mas sem esquecer o que importa.",
+      "O dia foi longo, mas estamos quase lá. Vamos finalizar juntos?"
+    ],
+    morning: [
+      "Bom dia! Vamos começar o dia com energia!",
+      "Que tal resolver suas tarefas matinais agora? Vamos lá!",
+      "Amanheceu! Hora de fazer coisas incríveis acontecerem"
+    ],
+    afternoon:[
+      "Boa tarde! Já conquistou algo hoje? Vamos juntos!",
+      "Ainda há tempo para fazer este dia ser produtivo!",
+      "Aproveite a tarde para seguir com seus planos"
+    ],
+    night:[
+      "Boa noite! Que tal finalizar o dia com chave de ouro?",
+      "Hora de encerrar o dia com tranquilidade. Vamos?",
+      "Antes de descansar, vamos cuidar do que falta hoje"
+    ]
+  },
   presentation: {
+    im: "Eu sou",
     subtitle: 'Desenvolvedor de Software',
     description: 'Sou um desenvolvedor de software com paixão por criar soluções inovadoras. Tenho um forte interesse em desenvolvimento web e estou sempre buscando novos desafios.'
   },
@@ -21,12 +47,17 @@ export const pt_br = {
   projects: {
     view: 'Ver mais',
     title: 'Projetos',
+    notFound: 'Ops! Projeto não encontrado',
+  },
+  button:{
+    return: 'Voltar',
   },
   confirmDelition: {
     title: 'Você tem certeza absoluta?',
     description: 'Esta ação não pode ser desfeita. Isso excluirá permanentemente os dados deste projeto de nossos servidores.',
     cancel: 'Cancelar',
-    confirm: 'Deletar'  },
+    confirm: 'Deletar'
+  },
   tableOfContents: {
     presentation: 'Apresentação',
     projects: 'Projetos',
