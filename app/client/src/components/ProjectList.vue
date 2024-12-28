573bf0527495195c7e22b71bdeff2c6c3d1e2458
Sander <82063944+Alexsander33set@users.noreply.github.com>
2024-12-28 13:09:15 -0300

    client component(projects list): Add image and limit description lines

--- app/client/src/components/ProjectList.vue
+++ app/client/src/components/ProjectList.vue
@@ -57,43 +57,52 @@ const filter = {
 
       <template v-if="projects.length > 0">
         <div v-for="(project, index) in props.projects" :key="index"
-        class="shadow-[0_0px_3px_0px_rgba(127,127,127,0.5)] p-4 rounded-md">
-          <h3 class="text-lg capitalize">
-            {{ project.name }}
-          </h3>
-          <p class="mt-2 whitespace-pre-wrap line-clamp-2"
-            v-html="project.description">
-          </p>
-          <div class="flex gap-2 mt-2">
-            <Badge class="capitalize leading-normal inline-flex gap-2"
-              v-for="(tech, i) in project.technologies"
-              :key="i">
-              <Icon v-if="filter[tech]" :icon="filter[tech]" />
-              {{ tech }}
-            </Badge>
+        class="shadow-[0_0px_3px_0px_rgba(0,0,0,0.3)] p-4 rounded-md flex gap-4">
+          <div>
+            <!-- <Skeleton id="getthis" class="h-[180px] w-[220px]"/> -->
+            <img class="object-cover h-[180px] w-[220px]"
+              :src=" project.image || 'no-image.png'"
+              alt="">
+          </div>
+          <div class="flex-1">
+            <h3 class="text-lg capitalize">
+              {{ project.name }}
+            </h3>
+            <p class="mt-2 whitespace-pre-wrap line-clamp-2"
+              v-html="project.description">
+            </p>
+            <div class="flex gap-2 mt-2">
+              <Badge class="capitalize leading-normal inline-flex gap-2"
+                v-for="(tech, i) in project.technologies"
+                :key="i">
+                <Icon v-if="filter[tech]" :icon="filter[tech]" />
+                {{ tech }}
+              </Badge>
+            </div>
+            <div class="flex justify-start gap-2 mt-6">
+              <RouterLink :to="'/project/'+project._id">
+                <Button variant="outline" class="cursor-pointer">
+                  {{ $t('projects.view') }}
+                </Button>
+              </RouterLink>
+              <template v-if="!project.is_private">
+                <a v-if="project.github" :href="project.github" target="_blank" rel="noopener noreferrer" class="cursor-pointer">
+                  <Button variant="outline"><Icon icon="mdi:github" /></Button>
+                </a>
+              </template>
+              <a v-if="project.url" :href="project.url" target="_blank" rel="noopener noreferrer" class="cursor-pointer">
+                <Button variant="outline"><Icon icon="akar-icons:globe" /></Button>
+              </a>
+            </div>
           </div>
 
-          <div class="flex justify-start gap-2 mt-6">
-            <RouterLink :to="'/project/'+project.slug">
-              <Button variant="outline" class="cursor-pointer">
-                {{ $t('projects.view') }}
-              </Button>
-            </RouterLink>
-            <template v-if="!project.is_private">
-              <a v-if="project.github" :href="project.github" target="_blank" rel="noopener noreferrer" class="cursor-pointer">
-                <Button variant="outline"><Icon icon="mdi:github" /></Button>
-              </a>
-            </template>
-            <a v-if="project.url" :href="project.url" target="_blank" rel="noopener noreferrer" class="cursor-pointer">
-              <Button variant="outline"><Icon icon="akar-icons:globe" /></Button>
-            </a>
-          </div>
+
         </div>
       </template>
       <template v-if="isLoading">
         <div class="shadow-[0_0px_3px_0px_rgba(0,0,0,0.3)] p-4 rounded-md flex gap-4">
           <div class="project-image">
-            <Skeleton class="h-[180px] w-[180px]"/>
+            <Skeleton class="h-[180px] w-[220px]"/>
           </div>
           <div class="text flex-1">
             <Skeleton class="h-6 w-[250px]"/>
